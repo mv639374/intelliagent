55620dbda8b9037582822113af8eafa20338cdc3
mv639374 <mviitb25@gmail.com>
2025-10-08 21:22:11 +0530

    Twelfth Commit

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // During development, proxy API requests to the backend server
+//   // to avoid CORS issues.
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination: "http://localhost:8000/api/:path*", // Proxy to Backend
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+
   // During development, proxy API requests to the backend server
   // to avoid CORS issues.
   async rewrites() {
